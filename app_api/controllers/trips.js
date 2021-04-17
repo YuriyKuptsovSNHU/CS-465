b9fb838e6996505daa9f6b0785ebc9f08499c20e
Yuriy <yuriy.kuptsov@snhu.edu>
2021-04-17 12:24:50 -0400

    Module 6 changes

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -31,7 +31,85 @@ const tripsFindByCode = async(req, res) => {
 		});
 };
 
+const deleteTripByCode =  async(req, res) => {
+	console.log('deleteTripByCode(' + req.params.tripCode + ')');
+	Model
+		.remove({'code': req.params.tripCode})
+		.exec((err, trip) => {
+			if (! trip) {
+				console.log("Trip not found!");
+				return res.status(404).json({"message": "trip not found"});
+			} else if (err) {
+				console.log("Error: " + err);
+				return res.status(404).json(err);
+			} else {
+				return res.status(200).json(trip);
+			}
+		});
+};
+
+const tripsAddTrip = async (req, res) => {
+	Model.create({
+		code: req.body.code,
+		name: req.body.name,
+		length: req.body.length,
+		start: req.body.start,
+		resort: req.body.resort,
+		perPerson: req.body.perPerson,
+		image: req.body.image,
+		description: req.body.description
+	},
+	(err, trip) => {
+		if (err) {
+			return res.status(400).json(err);	// bad request
+		} else {
+			return res.status(201).json(trip);	// created
+		}
+	});
+};
+
+const tripsUpdateTrip = async (req, res) => {
+	console.log(req.body);
+	Model
+		.findOneAndUpdate({ 'code': req.params.tripCode }, {
+			code: req.body.code,
+			name: req.body.name,
+			length: req.body.length,
+			start: req.body.start,
+			resort: req.body.resort,
+			perPerson: req.body.perPerson,
+			image: req.body.image,
+			description: req.body.description
+		}, { new: true })
+		.then(trip => {
+			if (!trip) {
+				return res
+					.status(404)
+					.send({
+						message: "Trip not found with code "
+							+ req.params.tripCode
+					});
+			}
+			res.send(trip);
+		}).catch(err => {
+			if (err.kind === 'ObjectId') {
+				return res
+					.status(404)
+					.send({
+						message: "Trip not found with code "
+							+ req.params.tripCode
+					});
+			}
+			return res
+				.status(500) // server error
+				.json(err);
+		});
+}
+
 module.exports = {
 	"tripList": tripsList,
-	"tripsFindByCode": tripsFindByCode
+	"tripsFindByCode": tripsFindByCode,
+	"deleteTripByCode": deleteTripByCode,
+	"tripsAddTrip": tripsAddTrip,
+	"tripsUpdateTrip": tripsUpdateTrip
 };
--- app_api/routes/api.js
+++ app_api/routes/api.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const controller= require('../controllers/trips');
 /* GET home page. */
-router.route('/trips').get(controller.tripList);
+router.route('/trips')
+    .get(controller.tripList)
+    .post(controller.tripsAddTrip)
+    .put(controller.tripsUpdateTrip);
+router.route('/trips/delete/:tripCode').delete(controller.deleteTripByCode);
 router.route('/trips/:tripCode').get(controller.tripsFindByCode);
 module.exports = router;
